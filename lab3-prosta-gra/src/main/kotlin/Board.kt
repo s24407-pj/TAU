9d2065c4d9835593b4074370cd6313034a3a6d81
Michal <s24407@pjwstk.edu.pl>
2024-11-20 21:44:45 +0100

    refactor fun names

--- lab3-prosta-gra/src/main/kotlin/Board.kt
+++ lab3-prosta-gra/src/main/kotlin/Board.kt
@@ -1,25 +1,29 @@
 package pl.example
 
 import kotlin.math.abs
+import kotlin.random.Random
 
 class Board(private val width: Int, private val height: Int) {
-    private val board = generateBoard()
+    val board = generateBoard()
     lateinit var startPoint: Pair<Int, Int>
     lateinit var endPoint: Pair<Int, Int>
 
+    init {
+        require(width > 4 && height > 4) { "Width and height must be greater than 4" }
+    }
+
     fun print() {
-        for (i in 0 .. height+1) {
-            for (j in 0 .. width+1) {
-                print(board[i][j] + " ")
-            }
-            println()
+        val builder = StringBuilder()
+        for (row in board) {
+            builder.append(row.joinToString(" ")).append("\n")
         }
+        println(builder)
     }
 
     private fun generateBoard(): Array<Array<Char>> {
         val board = Array(height + 2) { Array(width + 2) { ' ' } }
         addBorder(board)
-        generateStartAndEndPoint(board)
+        initializeStartAndEndPoints(board)
         generateObstacles(board)
         return board
     }
@@ -27,18 +31,18 @@ class Board(private val width: Int, private val height: Int) {
     private fun generateObstacles(board: Array<Array<Char>>) {
         for (i in 1..height) {
             for (j in 1..width) {
-                if (Math.random() < 0.3 && board[i][j] != 'A' && board[i][j] != 'B') {
+                if (Random.nextDouble() < 0.35 && board[i][j] != 'A' && board[i][j] != 'B') {
                     board[i][j] = 'X'
                 }
             }
         }
     }
 
-    private fun generateStartAndEndPoint(board: Array<Array<Char>>) {
-        startPoint = generateEdgePoint()
+    private fun initializeStartAndEndPoints(board: Array<Array<Char>>) {
+        startPoint = generateRandomEdgePoint()
 
         do {
-            endPoint = generateEdgePoint()
+            endPoint = generateRandomEdgePoint()
         } while (areAdjacent(startPoint, endPoint))
 
         board[startPoint.second][startPoint.first] = 'A' //pytanie czy dobrze
@@ -55,8 +59,8 @@ class Board(private val width: Int, private val height: Int) {
         }
     }
 
-    private fun generateEdgePoint(): Pair<Int, Int> {
-        return when ((Math.random() * 4).toInt()) {
+    private fun generateRandomEdgePoint(): Pair<Int, Int> {
+        return when ((0..3).random()) {
             0 -> Pair((1 .. width ).random(), 1) // Top edge
             1 -> Pair((1 .. width ).random(), height) // Bottom edge
             2 -> Pair(1, (1 .. height).random()) // Left edge
@@ -64,7 +68,7 @@ class Board(private val width: Int, private val height: Int) {
         }
     }
 
-    private fun areAdjacent(p1: Pair<Int, Int>, p2: Pair<Int, Int>): Boolean {
+    fun areAdjacent(p1: Pair<Int, Int>, p2: Pair<Int, Int>): Boolean {
         val dx = abs(p1.first - p2.first)
         val dy = abs(p1.second - p2.second)
         return dx + dy <= 1
